7430a9a309a543a553c573f8be07d36f33ddfe79
Jaehong Nam <57509978+nam3762@users.noreply.github.com>
2024-11-24 17:00:27 +0900

    [Gold V] Title: 토마토, Time: 596 ms, Memory: 158712 KB -BaekjoonHub

--- node.js/백준/Gold/7576. 토마토/토마토.js
+++ node.js/백준/Gold/7576. 토마토/토마토.js
@@ -1,20 +1,19 @@
 const fs = require('fs');
 const input = fs.readFileSync(0, 'utf8').toString().trim().split('\n');
-const [N, M] = input[0].trim().split(' ').map(Number);
+const [N, M] = input[0].split(' ').map(Number);
 const map = Array.from({length: M}, () => Array(N).fill(0));
-const visited = Array.from({length: M}, () => Array(N).fill(false));
 
 // 시작 지점 좌표
-let start_position = [];
+const queue = [];
 // 안 익은 토마토 개수
 let nomato = 0;
 for (let i = 1; i <= M; i++) {
-    const row = input[i].trim().split(' ').map(Number);
+    const row = input[i].split(' ').map(Number);
     for (let j = 0; j < N; j++) {
         map[i - 1][j] = row[j];
         if (row[j] === 1) {
             // 시작 지점 좌표를 배열에 저장
-            start_position.push([i - 1, j]);
+            queue.push([i - 1, j, 0]);
         } else if (row[j] === 0) {
             // 안 익은 토마토 카운팅
             nomato++;
@@ -27,43 +26,31 @@ if (nomato === 0) {
     console.log(0);
 } else {
     // 최대 depth = 최소 날짜
-    const day = BFS(start_position);
+    const day = BFS();
 
     // 안 익은 토마토가 1개라도 있으면 -1, 그렇지 않으면 day 출력
     const result = nomato ? -1 : day;
     console.log(result);
 }
 
-function BFS(start_position) {
+function BFS() {
     const dx = [-1, 1, 0, 0];
     const dy = [0, 0, -1, 1];
     let maxDepth = 0;
-    const queue = [];
     let index = -1;
-    for (const [x, y] of start_position) {
-        // 시작 지점에서 한 번에 BFS 실행
-        visited[x][y] = true;
-        queue.push([x, y, 0]);
-    }
-    while (queue.length > 0) {
-        index++;
-        const [currentX, currentY, depth] = queue[index];
-        // JavaScript Array의 shift() 메서드는 O(n)이라 느림!
-        // 따라서 인덱스로 관리
-        // const [currentX, currentY, depth] = queue.shift();
+
+    while (index < queue.length - 1) {
+        const [currentX, currentY, depth] = queue[++index];
+        // JavaScript Array의 shift() 메서드는 O(n)이라 느림! -> 인덱스로 관리
         maxDepth = Math.max(maxDepth, depth);
         for (let i = 0; i < 4; i++) {
             const nextX = dx[i] + currentX;
             const nextY = dy[i] + currentY;
-            if (isInside(nextX, nextY) && !visited[nextX][nextY] && map[nextX][nextY] === 0) {
-                visited[nextX][nextY] = true;
+            if (isInside(nextX, nextY) && map[nextX][nextY] === 0) {
                 map[nextX][nextY] = 1;
                 nomato--;
                 queue.push([nextX, nextY, depth + 1]);
             }
-        }
-        if (index >= queue.length - 1) {
-            break;
         }
     }
     return maxDepth;
