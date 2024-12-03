2c34ec3530c3f6af5efac9b74c12a2b537755f5a
JH Nam <57509978+nam3762@users.noreply.github.com>
2024-12-04 00:06:28 +0900

    [Silver I] Title: 숨바꼭질, Time: 440 ms, Memory: 97120 KB -BaekjoonHub

---
+++ node.js/백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').toString().trim().split('\n');
+const [N, K] = input[0].split(' ').map(Number);
+if (N > K) {
+    console.log(Math.abs(K - N));
+} else {
+    console.log(BFS(N));
+}
+
+function BFS(start) {
+    const queue = [];
+    queue.push([start, 0]);
+    const set = new Set();
+    let index = -1;
+    while (true) {
+        const [currentNumber, currentDepth] = queue[++index];
+        if (currentNumber === K) {
+            return currentDepth;
+        }
+        if (!set.has(currentNumber)) {
+            set.add(currentNumber);
+            queue.push([currentNumber + 1, currentDepth + 1]);
+            queue.push([currentNumber - 1, currentDepth + 1]);
+            if (currentNumber * 2 <= 200000) {
+                queue.push([currentNumber * 2, currentDepth + 1]);
+            }
+        }
+    }
+}
